eaa5785bab10328ff1522d96afdd3506695c0930
Matthias Christenson <matchristenson@gmail.com>
2020-02-20 23:30:22 -0500

    changes to run.py

--- run.py
+++ run.py
@@ -3,18 +3,17 @@
 
 from loris import conn, config
 
-conn()
-
-from loris.app.app import app
-
-from flask import Flask
-
-
-'''app.run(
-    port=1234, debug=False,
-    host='0.0.0.0'
-    )
-'''
 if __name__ == "__main__":
-    from waitress import serve
-    serve(app, port=1234,host='0.0.0.0')
+
+    conn()
+
+    from loris.app.app import app
+
+    app.run(
+        port=1234, debug=False,
+        host='0.0.0.0', 
+        ssl_context='adhoc'
+    )
+ 
+    #from waitress import serve
+    #serve(app, port=1234,host='0.0.0.0')
