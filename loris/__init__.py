6d749b8a9055802685ddef1d60933aa448908b38
gucky92 <gucky@gucky.eu>
2020-03-11 16:38:28 -0400

    init change

--- loris/__init__.py
+++ loris/__init__.py
@@ -7,12 +7,14 @@ from loris.settings import Config
 import loris.database as db
 import loris.database.schema as schema
 from loris.errors import LorisError
-import loris.dataframe as df
 
 os.environ['DJ_SUPPORT_ADAPTED_TYPES'] = "TRUE"
 
 config = Config.load()
 conn = config.conn
+
+
+import loris.dataframe as df
 
 
 __all__ = [
